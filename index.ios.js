e9368ab15da1fd13c34d755bad3d19c601b91987
Yosuke Saito <yosuke.saito1202@gmail.com>
2017-06-19 00:21:43 +0900

    save login state in async storage

--- index.ios.js
+++ index.ios.js
@@ -5,10 +5,13 @@
  */
 
 import React, { Component } from 'react'
-import { AppRegistry } from 'react-native'
+import { AppRegistry, AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { compose, createStore, applyMiddleware, combineReducers } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import persistState from 'redux-localstorage'
+import adapter from 'redux-localstorage/lib/adapters/AsyncStorage'
+import filter from 'redux-localstorage-filter'
 import AppReducer from './src/reducers'
 import AppWithNavigationState from './src/navigators/AppNavigator'
 
@@ -16,17 +19,26 @@ console.disableYellowBox = true
 
 const middlewares = [thunkMiddleware]
 
+const storage = compose(
+  filter(['auth'])
+)(adapter(AsyncStorage))
+
 if (process.env.NODE_ENV === 'development') {
   const { logger } = require('redux-logger')
   middlewares.push(logger)
 }
 
-export default class ReactNavigationReduxIntegration extends Component {
-  store = createStore(AppReducer, {}, applyMiddleware(...middlewares))
+const enhancer = compose(
+  applyMiddleware(...middlewares),
+  persistState(storage, 'auth')
+)
 
+const store = createStore(AppReducer, {}, enhancer)
+
+export default class ReactNavigationReduxIntegration extends Component {
   render() {
     return (
-      <Provider store={this.store}>
+      <Provider store={store}>
         <AppWithNavigationState />
       </Provider>
     )
--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { connect } from 'react-redux'
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native'
+import { NavigationActions } from 'react-navigation'
 import { Container, Header, Left, Body, Title, Right, Button, Content, Icon } from 'native-base'
 
 class SplashScreen extends React.Component {
   constructor(props) {
     super(props)
+    AsyncStorage.getItem('auth', (error, result) => {
+      console.log(result)
+    })
+  }
+
+  componentDidMount() {
+    const { auth } = this.props
+    const { isLoggedIn } = auth
+    console.log(`isLoggedIn = ${isLoggedIn}`)
+    let routeName = 'Login'
+    if (isLoggedIn) {
+      routeName = 'Main'
+    }
+    const action = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName })
+      ]
+    })
+    setTimeout((splash, action) => {
+      splash.props.navigation.dispatch(action)
+    }, 1000, this, action)
   }
 
   render() {
@@ -18,4 +42,8 @@ class SplashScreen extends React.Component {
   }
 }
 
-export default SplashScreen
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps)(SplashScreen)
